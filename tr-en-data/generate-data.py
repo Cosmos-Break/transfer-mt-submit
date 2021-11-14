4ed61dc52eb62545b73903d777ddb0ac0d6e0b65
Cosmos-Break <438854233@qq.com>
2021-11-14 16:08:28 +0800

    push

--- tr-en-data/generate-data.py
+++
@@ -1,7 +0,0 @@
-from xml.etree.ElementTree import parse
-from bs4 import BeautifulSoup
-# doc = parse('IWSLT18.TED.dev2018.eu-en.en.xml')
-for line in open('tr-en-data/newstest2017-tren-src.tr.sgm'):
-    if line.startswith('<seg id='):
-        line = BeautifulSoup(line, "html.parser").text
-        print(line.strip())
